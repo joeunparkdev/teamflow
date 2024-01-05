b60edfd358cb27f97c3a4b0e05ecdb024f2e49d5
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2024-01-05 15:18:00 +0900

    setup

--- dist/auth/dtos/sign-in.dto.d.ts
+++ dist/auth/dtos/sign-in.dto.d.ts
@@ -1,5 +1,5 @@
 import { User } from 'src/user/entities/user.entity';
-declare const SignInDto_base: import("@nestjs/common").Type<Pick<User, "email" | "password">>;
+declare const SignInDto_base: import("@nestjs/common").Type<Pick<User, "password" | "email">>;
 export declare class SignInDto extends SignInDto_base {
 }
 export {};
