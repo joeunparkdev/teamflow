a07773f4f86c16feaa3165310d83801c01089371
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2024-01-07 21:10:40 +0900

    feat:update my info debugging

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,17 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
   HttpStatus,
+  Put,
   Request,
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UpdateMyInfoDto } from 'src/auth/dtos/update-my-info.dto';
 
 @ApiTags('사용자')
 @Controller('users')
@@ -34,6 +37,26 @@ export class UserController {
       data,
     };
   }
+
+  /**
+   * 내 정보 수정
+   * @param req
+   * @returns
+   */
+   @ApiBearerAuth()
+   @UseGuards(JwtAuthGuard)
+   @Put('/me')
+   async updateMyInfo(@Request() req, @Body() updateMyInfoDto: UpdateMyInfoDto) {
+     const userId = req.user.id;
+ 
+     const data = await this.userService.updateMyInfo(userId,updateMyInfoDto);
+ 
+     return {
+       statusCode: HttpStatus.OK,
+       message: '내 정보 수정에 성공했습니다.',
+       data,
+     };
+   }
 
   /**
    * 회원 탈퇴
