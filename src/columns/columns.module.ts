436f45ff16d1eb75e06a81f9cbaf962d36dd1354
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2024-01-09 20:19:16 +0900

    feat: refactoring

--- src/columns/columns.module.ts
+++ src/columns/columns.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ColumnsController } from './Columns.controller';
-import { ColumnsService } from './columns.service'; 
-import { Columns } from './entities/Columns.entity';
+import { ColumnsController } from './columns.controller';
+import { ColumnsService } from './columns.service';
+import { Columns } from './entities/columns.entity';
 import { User } from 'src/user/entities/user.entity';
 import { UserModule } from '../user/user.module';
 
