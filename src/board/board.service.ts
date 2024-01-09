53a2c258fb21a6dcfd9fbc3cb9125db34b9da1b0
IMCORIN2 <k_yoon1121@naver.com>
2024-01-09 23:08:03 +0900

    feat:refactoring

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,5 +1,6 @@
 import {
   BadRequestException,
+  ForbiddenException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -53,13 +54,14 @@ export class BoardService {
     });
 
     if (!user) {
-      throw new NotFoundException('보드 수정 권한이 없는 유저입니다.');
+      throw new ForbiddenException('보드 수정 권한이 없는 유저입니다.');
     }
 
-    const updatedBoard = await this.findBoardById(boardId);
+    const Board = await this.findBoardById(boardId);
+
     const { name, backgroundColor, description } = updateBoardDto;
 
-    await this.boardRepository.update(boardId, {
+    const updatedBoard = await this.boardRepository.update(boardId, {
       name,
       backgroundColor,
       description,
@@ -93,7 +95,7 @@ export class BoardService {
     });
 
     if (!user) {
-      throw new BadRequestException('보드를 생성한 유저만 삭제가 가능합니다.');
+      throw new ForbiddenException('보드를 생성한 유저만 삭제가 가능합니다.');
     }
     const deletedBoard = await this.boardRepository.delete({ id: boardId });
 
@@ -160,7 +162,6 @@ export class BoardService {
     });
 
     const user = await this.userRepository.findOne({ where: { email } });
-    console.log(1);
 
     if (!user) {
       throw new NotFoundException('존재하지 않는 유저입니다.');
@@ -173,22 +174,17 @@ export class BoardService {
     if (!savedCode) {
       throw new NotFoundException('인증 코드가 존재하지 않습니다.');
     }
-    console.log(2);
     if (Date.now() > savedCode.expiry.getTime()) {
       await this.verificationCodeRepository.delete({ email });
       throw new BadRequestException('인증 시간이 초과되었습니다.');
     }
-    console.log(3);
     if (savedCode.code === code) {
       // board-user에 userId와 boardId 집어넣기
-
-      console.log(user);
       const userId = user.id;
       const boardUser = await this.boardUserRepository.save({
         userId,
         boardId,
       });
-      console.log(4);
       //boardUser row값 보내기;
       return boardUser;
     }
