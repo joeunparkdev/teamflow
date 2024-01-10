6096550c2003029ff0e3913e883e712ea2e20564
IMCORIN2 <k_yoon1121@naver.com>
2024-01-10 13:57:22 +0900

    feat: refactoring

--- src/board/dto/invitation.dto.ts
+++ src/board/dto/invitation.dto.ts
@@ -2,6 +2,9 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class InvitationDto {
   @IsEmail()
-  @IsNotEmpty({ message: '초대할 사람의 이메일을 작성해주세요.' })
+  @IsNotEmpty({ message: '초대받을 사람의 이메일을 작성해주세요.' })
   memberEmail: string;
+
+  @IsNotEmpty({ message: '초대할 boardId를 입력해주세요.' })
+  boardId: number;
 }
